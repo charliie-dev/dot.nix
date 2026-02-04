a5d350da652fce3b5e4169a6c250f2fe77e8cff4
charliie-dev <mail@charliie.dev>
2026-02-05 02:02:22 +0800

    chore: rekey

--- secrets.nix
+++ secrets.nix
@@ -15,9 +15,9 @@ let
   nate-test = "age10yxsamlz7rtc2lq4g5wtjdhktrycz4hlsyj7hrv86lrqaasumprsaskk0r";
 
   pg-proxy-dev = "age1mg3w0fvrcxn5hxea5wljdqshel3l2lykv9l62ux2nfnyt3ck4yzq6jq4v3";
-  pg-primary-dev = "age17dzrdlw72pk9ne8rumparvmxg3tvrt5x3j8gen0tn3l7yptvr5jq9t7xwp";
-  pg-replica1-dev = "age12xz85y9kya03wug9zgej04lfkta2lr6mfa6vlzjte4a722flygzqs0cyc3";
-  pg-replica2-dev = "age1lqex949pjx6r05xe72majgps0wkerxksmekydgv292daprvfrdjq0g4eht";
+  pg-primary-dev = "age19tmhkq52a077pnrhu6svd05vm45wuffxfmkmgzdczgl03f3z5uusde3myg";
+  pg-replica1-dev = "age1qrk5dqfnzhd9sy5ujd0p3q6gky0hztn3mrv5gzcj9yutcdu2552qflgy6s";
+  pg-replica2-dev = "age1a8gvyt8zw5qm0t752cezs5e7dfmymxqlqvw59vgu9kjnhj0aks2q3e937e";
   hosts = [
     pg-proxy-dev
     pg-primary-dev
