159989a39daaeac8e9389729edc0b36966603748
charliie-dev <mail@charliie.dev>
2026-02-22 17:19:57 +0800

    feat(apps._common): add `nixd` and `nil` config

--- modules/apps/_common.nix
+++ modules/apps/_common.nix
@@ -23,6 +23,8 @@
     ### nix-build --log-format internal-json -v |& nom --json
     nix-tree # Interactively browse dependency graphs of Nix derivations.
     dix # A blazingly fast tool to diff Nix related things.
+    nixd # Nix language server
+    nil # Yet another language server for Nix
 
   ];
 }
