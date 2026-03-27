f80ec85ced3a711e8fcf12d58e472335b39c7aee
charliie-dev <mail@charliie.dev>
2026-03-28 00:21:45 +0800

    fix(gpu): remove targets.genericLinux.gpu to fix CI eval failure
    The gpu and nixGL options are an unsupported combination that caused
    `impure-nvidia-version-file` to fail in CI (no nvidia driver on runner).
    nixGL alone handles GPU wrapping without requiring host nvidia detection.

--- modules/targets/genericLinux-gpu.nix
+++ modules/targets/genericLinux-gpu.nix
@@ -2,16 +2,6 @@
 {
   genericLinux = {
     enable = true;
-    gpu = {
-      enable = true;
-      # https://home-manager-options.extranix.com/?query=targets.genericLinux.gpu.nvidia&release=master
-      nvidia = {
-        enable = true;
-        version = "580.95.05"; # This version **must** match the version of the driver used by the host OS.
-        # get sha256: nix store prefetch-file https://download.nvidia.com/XFree86/Linux-x86_64/@VERSION@/NVIDIA-Linux-x86_64-@VERSION@.run
-        sha256 = "sha256-gJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
-      };
-    };
     # https://home-manager-options.extranix.com/?query=targets.genericLinux.nixGL&release=master
     nixGL = {
       inherit (nixgl) packages;
