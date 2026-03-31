700fe438e4c3b7d07045f53f9e43a2545f980485
charliie-dev <mail@charliie.dev>
2026-03-31 13:36:15 +0800

    feat(darwin-top): add colima and docker-client packages

--- modules/apps/roles/darwin-top.nix
+++ modules/apps/roles/darwin-top.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 {
   packages = with pkgs; [
+    colima # Container runtimes on macOS with minimal setup
+    docker-client # Docker CLI (client only, no daemon)
     macpm # Perf monitoring CLI tool for Apple Silicon, ; previously named 'asitop'
   ];
 }
