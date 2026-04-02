eb0f3bf2ed2407c4c9451bad260ea6170d28b43e
charliie-dev <mail@charliie.dev>
2026-04-02 15:48:36 +0800

    chore(dag): fix `topgradeCopy`
    Signed-off-by: charliie-dev <mail@charliie.dev>

--- modules/core.nix
+++ modules/core.nix
@@ -87,7 +87,7 @@ lib.mkMerge [
         '';
         topgradeCopy = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
           if [ ! -f ${config.xdg.configHome}/topgrade.d/disable.toml ]; then
-            cd ${config.xdg.configHome}/home-manager/conf.d/
+            mkdir -r ${config.xdg.configHome}/topgrade.d
             cp "${src}/conf.d/topgrade/disable.toml" ${config.xdg.configHome}/topgrade.d/disable.toml
           fi
         '';
