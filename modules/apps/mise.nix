2940ea3b18258546a32ea7dc13d714ffb26b66c9
charliie-dev <mail@charliie.dev>
2026-05-08 04:40:04 +0800

    chore(mise): add caveman-shrink npm tool

--- modules/apps/mise.nix
+++ modules/apps/mise.nix
@@ -19,6 +19,7 @@
         "npm:@hackmd/hackmd-cli" = "latest";
         "npm:@readwise/cli" = "latest";
         "npm:@sliday/tamp" = "latest";
+        "npm:caveman-shrink" = "latest";
         "npm:ctx7" = "latest";
         "go:github.com/go-delve/delve/cmd/dlv" = "latest";
         "go:mvdan.cc/gofumpt" = "latest";
