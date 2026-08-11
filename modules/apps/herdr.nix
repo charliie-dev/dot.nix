73de0071f5bc099f86dd49381e43c18e374a3365
charliie-dev <mail@charliie.dev>
2026-08-11 12:46:48 +0800

    chore(herdr): prevent idle spike notify
    Signed-off-by: charliie-dev <mail@charliie.dev>

--- modules/apps/herdr.nix
+++ modules/apps/herdr.nix
@@ -28,7 +28,7 @@
           # terminal:走 OSC 9/99 請 ghostty 發桌面通知(可拿到 ghostty 的通知音)
           delivery = "terminal";
           # 延遲送出:到期時 pane 仍是同一狀態才通知,濾掉 subagent 結束造成的瞬間 idle
-          delay_seconds = 30;
+          delay_seconds = 120;
           # clipboard toast 只吃 top-center / bottom-center,沒有四角
           clipboard.position = "top-center";
         };
